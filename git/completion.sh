c3a4a2d96da53b23bf58e04891a08831f38edf08
Dave Reed <infinity88@gmail.com>
2022-05-04 10:50:12 -0700

    Delete git directory

--- git/completion.sh
+++
@@ -1,8 +0,0 @@
-completion=/usr/share/git/completion/git-completion.bash
-gitprompt=/usr/share/git/completion/git-prompt.sh
-
-[[ -f $completion ]] && . $completion
-[[ -f $gitprompt ]] && . $gitprompt
-
-unset completion
-unset gitprompt
